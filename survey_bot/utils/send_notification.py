06ba113b560f21c86d3aa42c198fdef2c93b46ec
jojopko <gamerat.github@gmail.com>
2024-05-04 10:20:15 +0300

    Реализована оправка сообщений зарегистрированным пользователям

--- survey_bot/utils/send_notification.py
+++ survey_bot/utils/send_notification.py
@@ -1,15 +1,34 @@
 import asyncio
+from logging import getLogger
 
 from telegram import Bot
+
 from survey_bot.const import BOT_TOKEN
+
+logger = getLogger(__name__)
 
 bot = Bot(BOT_TOKEN)
 
 
-async def send_notification(user: int):
-    print('hi')
-    await bot.sendMessage(user, "hello!")
+async def send_notification(message: str, user: int):
+    await bot.sendMessage(user, message)
 
 
 async def send_notifications(message: str, users: list[int]):
-    result = [result for result in asyncio.as_completed(map(send_notification, users))]
+    """Отправляет сообщение списку пользователей"""
+    result = await asyncio.gather(
+        *asyncio.as_completed(map(
+            lambda user: send_notification(message, user),
+            users
+        )),
+        return_exceptions=True
+    )
+    fails = tuple(filter(
+        lambda x: isinstance(x, Exception),
+        result
+    ))
+
+    logger.info('Сообщения отправлены %s пользователям', len(result) - len(fails))
+    if len(fails):
+        logger.warning('%s сообщений не были доставлены', len(fails))
+        logger.debug(fails)
